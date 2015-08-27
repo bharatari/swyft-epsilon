0106270f229d7b86fb9a6aa6e8f52fa99424891a
Bharat Arimilli <bbarimilli@outlook.com>
2015-08-27 12:58:01 -0500

    New date-time-picker component for simple date and time entry

---
+++ SwyftOnline/app/components/date-time-picker.js
@@ -0,0 +1,26 @@
+import Ember from "ember";
+
+export default Ember.Component.extend({
+  	tagName: 'input',
+    attributeBindings: ['id', 'required', 'value', 'disabled', 'name', 'autofocus', 'type', 'readonly'],
+	type: 'text',
+    action: Ember.K, // Ember.K is a placeholder for the passed in action function
+                     // Action to fire on change
+    
+    didInitAttrs() {
+        if(this.get('initialValue')) {
+            this.set('value', moment(this.get('initialValue')).tz("America/New_York").format("MM-DD-YYYY hh:mm a Z"));
+        }
+    },
+    didInsertElement() {
+      this.on('change', function() {
+        this.set('value', $("#" + this.elementId).val());
+        this.processChange(this.get('value'));
+      });  
+    },
+    processChange(value) {
+        value = moment(value, "MM-DD-YYYY hh:mm a Z").toISOString();
+        const changeCallback = this.get('action');
+        changeCallback(value);  
+    }
+});
