884c61fc8f57ac68f4e101f295751dcdd3d6e155
Bharat Arimilli <bbarimilli@outlook.com>
2015-08-27 13:22:33 -0500

    Added Order CRUD

--- SwyftOnline/app/controllers/admin-orders.js
+++ SwyftOnline/app/controllers/admin-orders.js
@@ -1,14 +1,60 @@
 import Ember from "ember";
 import loginUtils from 'swyft-epsilon-online/utils/login-utils';
 import config from 'swyft-epsilon-online/config/environment';
+import AdminPageMixin from 'swyft-epsilon-online/mixins/admin-page';
 
-export default Ember.Controller.extend({
-    transactions: function() {
-        var array = this.get('model');
-        for(var i = 0; i < array.length; i++) {
-            array[i].displayDate = moment(array[i].createdAt).format("dddd, MMMM Do YYYY, h:mm:ss a");
+export default Ember.Controller.extend(AdminPageMixin, {
+    modelName: 'Order',
+    description: 'Add, update, and edit Orders.',
+    currentRoute: 'admin-orders',
+    queryParams: ['page', 'sort', 'sortType', 'filters', 'filterArray'],
+    page: 1,                                  
+    sort: 'createdAt',
+    sortType: 'DESC',
+    filterArray: [],
+    filtersChanged: function() {
+        this.set('filters', JSON.stringify(this.convertFilters(this.get('filterArray'))));
+    }.observes('filterArray'),
+    actions: {
+        delete: function(id) {
+            this.set('dialogTitle', 'Confirmation');
+            this.set('dialogBody', "You are attempting to permanently delete this order.");
+            this.set('displayDialog', true);
+            this.set('deleteId', id);
+        },
+        accept: function() {
+            var self = this;
+            if(this.get('deleteId')) {
+                Ember.$.ajax({
+                    url: config.routeLocation + "/api/admin/order/" + this.get('deleteId'),
+                    data: {
+                        user: {token: JSON.parse(localStorage.getItem(loginUtils.localStorageKey)).token}
+                    },
+                    type: "DELETE",
+                    success: function(response) {
+                        self.set('displayDialog', false);
+                        self.set('modalTitle', 'Order Deleted');
+                        self.set('modalBody', 'This order has been successfully deleted.');
+                        self.set('displayModal', true);
+                    },
+                    error: function(xhr, textStatus, error) {
+                        self.set('displayDialog', false);
+                        self.set('modalTitle', 'Error');
+                        self.set('modalBody', 'Something went wrong with your request.');
+                        self.set('displayModal', true);
+                    }
+                });
+            }
+        },
+        cancel: function() {
+            this.set('deleteId', 0);
+            this.set('displayDialog', false);
+        },
+        goToNewDeliveryLocation: function() {
+            this.transitionToRoute('admin-order', 0);
+        },
+        goToDeliveryLocation: function(id) {
+            this.transitionToRoute('admin-order', id);
         }
-        return array;
-    }.property('model'),
-    currentPage: 1
+    }
 });
