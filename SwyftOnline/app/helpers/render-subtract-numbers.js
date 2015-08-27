dabb79b570b43cbb9e53c0662f0eeed48ebc507a
Bharat Arimilli <bbarimilli@outlook.com>
2015-08-27 12:42:36 -0500

    render-subtract-numbers deprecated in favor of format-subtract-currency

--- SwyftOnline/app/helpers/render-subtract-numbers.js
+++
@@ -1,6 +0,0 @@
-import Ember from "ember";
-
-export default Ember.Helper.helper(function(params, hash) {
-    return "$" + math.chain(math.bignumber(params[1])).subtract(math.bignumber(params[0])).done().toString();
-});
-
