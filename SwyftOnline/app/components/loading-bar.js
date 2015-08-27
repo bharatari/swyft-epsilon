c48d17cf9d32160a6e98fb437895a9db2fb4f93f
Bharat Arimilli <bbarimilli@outlook.com>
2015-08-27 13:16:49 -0500

    UI refinements to consumer-facing pages

--- SwyftOnline/app/components/loading-bar.js
+++ SwyftOnline/app/components/loading-bar.js
@@ -1,17 +1,3 @@
 import Ember from "ember";
 
-export default Ember.Component.extend({
-    animate: function() {
-        var delay = this.get('animateDelay');
-        if(delay) {
-            setTimeout(function() {
-                $(".animate-in-body").removeClass('invisible').addClass('fadeIn');
-            }, delay);
-        }
-        else {
-            setTimeout(function() {
-                $(".animate-in-body").removeClass('invisible').addClass('fadeIn');
-            }, 500);
-        }
-    }.on('didInsertElement')
-});
+export default Ember.Component.extend({});
