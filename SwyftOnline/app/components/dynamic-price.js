826a78b3452debcf9d2142d3d345286597f46275
Bharat Arimilli <bbarimilli@outlook.com>
2015-08-27 13:00:44 -0500

    Bug fixes and re-factored code for dynamic-price component

--- SwyftOnline/app/components/dynamic-price.js
+++ SwyftOnline/app/components/dynamic-price.js
@@ -4,23 +4,21 @@ import loginUtils from 'swyft-epsilon-online/utils/login-utils';
 
 export default Ember.Component.extend({  
     classNames: ['inline'],
-    discountedAmount: function() {
-        return Math.round((this.get('price') - this.get('computedPrice')) * 100) / 100;
-    }.property('price', 'computedPrice'),
-    computedPrice: function() {
+    discountedAmount: Ember.computed('price', 'finalAmount', function() {
+        return Math.round((this.get('price') - this.get('finalAmount')) * 100) / 100;
+    }),
+    computePrice: Ember.observer('price', 'discount', function() {
         if(this.get('discount')) {
             this.set('finalAmount', this.get('price') * this.get('discount'));
-            return this.get('price') * this.get('discount');
         }
         else {
             this.set('finalAmount', this.get('price'));
-            return this.get('price');
         }
-    }.property('price', 'discount'),
-    valueChanged: function() {
+    }),
+    valueChanged: Ember.observer('value', 'valid', function() {
         var self = this;
-        if(this.get('dynamic') && this.get('value')) {
-            Ember.$.getJSON(config.routeLocation + "/api/coupon/token/" + this.get('value'), {
+        if(this.get('valid') && this.get('value')) {
+            Ember.$.getJSON(config.routeLocation + "/api/coupon/token/" + this.get('value').trim(), {
                 token: JSON.parse(localStorage.getItem(loginUtils.localStorageKey)).token.token, 
                 tokenId: JSON.parse(localStorage.getItem(loginUtils.localStorageKey)).token.id
             }).done(function(data) {
@@ -29,12 +27,11 @@ export default Ember.Component.extend({
                 }
                 else {
                     self.set('discount', data.discount);
-                    self.get('computedPrice');
                 } 
             });
         }
         else {
             this.set('discount', null);
         }
-    }.observes('value', 'dynamic')
+    })
 });
