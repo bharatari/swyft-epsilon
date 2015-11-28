20199119eaa8b7640135825a9401a1e87547db3c
Bharat Arimilli <bbarimilli@outlook.com>
2015-11-27 21:43:53 -0500

    Swyft Epsilon 4.1.0

--- SwyftOnline/app/routes/admin-about.js
+++ SwyftOnline/app/routes/admin-about.js
@@ -2,6 +2,6 @@
 import Ember from "ember";
 import config from 'swyft-epsilon-online/config/environment';
 import loginUtils from 'swyft-epsilon-online/utils/login-utils';
-import AdminRouteMixin from 'swyft-epsilon-online/mixins/admin-route';
+import DeliveryRouteMixin from 'swyft-epsilon-online/mixins/delivery-route';
 
-export default Ember.Route.extend(AdminRouteMixin, { });
+export default Ember.Route.extend(DeliveryRouteMixin, { });
