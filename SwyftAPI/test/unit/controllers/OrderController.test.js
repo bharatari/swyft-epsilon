0919de14b41cbcee1ecd14b26b84a874bb671e95
Bharat Arimilli <bbarimilli@outlook.com>
2016-01-11 09:24:03 -0600

    Minor changes

--- SwyftAPI/test/unit/controllers/OrderController.test.js
+++ SwyftAPI/test/unit/controllers/OrderController.test.js
@@ -3,12 +3,37 @@ var superagent = require('superagent');
 var assert = require('chai').assert;
 var expect = require('chai').expect;
 
-var sampleOrder = {"items":[{"name":"McCafé Coffee","id":"549a7c80f2ede1f4234eecf9","quantity":1,"restaurant":"mcdonalds","standardOptions":[],"options":[{"price":0.5,"name":"Medium","category":"Size"}],"extras":[],"attachedRequests":[],"price":1.7,"itemOptions":"Medium"}],"paymentType":"cash","deliveryLocation":"Main Street Hall","contactPhone":"6037937340","userId":"54231d2716903854156ad062","token":"","deliveryId":"56798eb1ef2b22f44f738535","user":{}};
-
 describe('OrderController', function() {
     describe('#processOrder()', function() {
         it('should process a valid order', function(done) {
+            var sampleOrder = {
+                "items": [
+                    {
+                        "name": "McCafé Coffee",
+                        "id": "549a7c80f2ede1f4234eecf9",
+                        "quantity": 1,
+                        "restaurant": "mcdonalds",
+                        "standardOptions": [],
+                        "options": [
+                            { "price":0.5, "name":"Medium", "category":"Size" }
+                        ],
+                        "extras": [],
+                        "attachedRequests": [],
+                        "price": 1.7,
+                        "itemOptions": "Medium"
+                    }
+                ],
+                "paymentType": "cash",
+                "deliveryLocation": "Main Street Hall",
+                "contactPhone": "6037937340",
+                "userId": "54231d2716903854156ad062",
+                "token": "",
+                "deliveryId": "56798eb1ef2b22f44f738535",
+                "user": {}
+            };
+
             var agent = superagent.agent();
+
             request(sails.hooks.http.app)
                 .post('/api/login')
                 .send({ username: "swyfttestmocha@exeter.edu", password: 'testing' })
@@ -19,10 +44,7 @@ describe('OrderController', function() {
                     sampleOrder.user.token = token;
 
                     agent.saveCookies(res.res);
-
-                    var req = request(sails.hooks.http.app)
-                                  .post('/api/order');
-
+                    var req = request(sails.hooks.http.app).post('/api/order');
                     agent.attachCookies(req);
 
                     req
