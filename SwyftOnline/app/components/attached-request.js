d84b0ef702305bb3a63ee313f03eb298fd08e01a
Bharat Arimilli <bbarimilli@outlook.com>
2016-01-18 13:55:05 -0600

    Edit components

--- SwyftOnline/app/components/attached-request.js
+++ SwyftOnline/app/components/attached-request.js
@@ -1,13 +1,10 @@
 /* jslint unused: false */
-import Ember from "ember";
+import Ember from 'ember';
 import itemUtils from 'swyft-epsilon-online/utils/item-utils';
 
-var observer = Ember.observer;
-var get = Ember.get;
-
-export default Ember.Component.extend({ 
-    displayChange: observer('item.itemOptions.@each.value', function(sender, key, value, rev) {
-        this.set('display', itemUtils.checkAttachedProperty(this.get('request'), this.get('item')));
-    }),
-    display: false
+export default Ember.Component.extend({
+  displayChange: Ember.observer('item.itemOptions.@each.value', () => {
+    this.set('display', itemUtils.checkAttachedProperty(this.get('request'), this.get('item')));
+  }),
+  display: false,
 });
