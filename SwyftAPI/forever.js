4d72cb367572bb8ffd5ef867535a99c63661d570
Bharat Arimilli <bbarimilli@outlook.com>
2016-01-23 15:50:35 -0600

    Minor change to forever.js

--- SwyftAPI/forever.js
+++ SwyftAPI/forever.js
@@ -2,8 +2,8 @@ var forever = require('forever-monitor');
 
 var child = new (forever.Monitor)('app.js');
 
-child.on('error', function(){
-    console.log("Swyft App is restarting due to an error.");
+child.on('error', function () {
+    console.log("Swyft Epsilon is restarting due to an error.");
 });
 
 
