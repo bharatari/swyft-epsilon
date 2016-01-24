c0c426bc1a70edbc0e194d0e76649e6bb32cac42
Bharat Arimilli <bbarimilli@outlook.com>
2016-01-24 11:26:42 -0600

    Beautify code to match master

--- SwyftAPI/api/controllers/CouponController.js
+++ SwyftAPI/api/controllers/CouponController.js
@@ -2,97 +2,102 @@ var Chance = require('chance');
 var chance = new Chance();
 
 module.exports = {
-    getDiscount: function(req, res) {
-        CouponService.getDiscount(req.params.code, function(err, discount) {
-            if (err || !discount) {
-                res.badRequest();
-            } else {
-                res.ok(discount);
-            }
-        });
-    },
-    checkToken: function(req, res) {
-        Token.findOne({ token: req.params.token }).exec(function(err, token) {
-            if(!token) {
-                return res.badRequest();
-            }
-            else if(token.hasBeenUsed) {
-                return res.badRequest();
-            }
-            else {
-                return res.ok();
-            }
-        });
-    },
-    checkCoupon: function(req, res) {
-        Coupon.findOne({ code: req.params.coupon }).exec(function(err, coupon) {
-            if(err || !coupon) {
-                return res.badRequest();
-            } else if(!coupon.isActive) {
-                return res.badRequest();
-            } else {
-                return res.ok();
-            }
-        });
-    },
-    getOpenTokens: function(req, res) {
-        Token.find({ hasBeenUsed: false }).exec(function(err, tokens) {
-            res.json(tokens);
-        });
-    },
-    createIndependentToken: function(req, res) {
-        var data = {
-            token: chance.guid(),
-            discount: req.body.discount,
-            comments: req.body.comments,
-            hasBeenUsed: false
-        }
-        Token.create(data).exec(function(err, token) {
-            if (err) {
-                return res.badRequest();
-            } else {
-                res.json(token);
-            }
-        });
-    },
-    createCoupon: function(req, res) {
-        var data = {
-            name: req.body.name,
-            discount: req.body.discount,
-            comments: req.body.comments,
-            code: req.body.code,
-            isActive: true
-        };
-        CouponService.checkDuplicates(req.body.code, function(err, result) {
-            if (err || !result) {
-                res.badRequest("DUPLICATE");
-            } else {
-                Coupon.create(data).exec(function(err, coupon) {
-                    if (err || !coupon) {
-                        res.badRequest("DATABASE_ERR");
-                    } else {
-                        res.ok(coupon);
-                    }
-                });
-            }
-        });
-    },
-    getToken: function(req, res) {
-        Token.findOne({ token: req.params.token }).exec(function(err, token) {
-            if(err || !token) {
-                res.badRequest();
-            }
-            else {
-                res.json(token);
-            }
-        });
-    },
-    getCouponMetadata: function(req, res) {
-        MetaService.getCouponMetadata(req.query.limit, req.query.where, function(result) {
-            res.json(result);
-        });
-    },
-    getCouponModel: function(req, res) {
-        res.json(new ModelService.Coupon());
+  getDiscount: function (req, res) {
+    CouponService.getDiscount(req.params.code, function (err, discount) {
+      if (err || !discount) {
+        res.badRequest();
+      } else {
+        res.ok(discount);
+      }
+    });
+  },
+  checkToken: function (req, res) {
+    Token.findOne({
+      token: req.params.token
+    }).exec(function (err, token) {
+      if (!token) {
+        return res.badRequest();
+      } else if (token.hasBeenUsed) {
+        return res.badRequest();
+      } else {
+        return res.ok();
+      }
+    });
+  },
+  checkCoupon: function (req, res) {
+    Coupon.findOne({
+      code: req.params.coupon
+    }).exec(function (err, coupon) {
+      if (err || !coupon) {
+        return res.badRequest();
+      } else if (!coupon.isActive) {
+        return res.badRequest();
+      } else {
+        return res.ok();
+      }
+    });
+  },
+  getOpenTokens: function (req, res) {
+    Token.find({
+      hasBeenUsed: false
+    }).exec(function (err, tokens) {
+      res.json(tokens);
+    });
+  },
+  createIndependentToken: function (req, res) {
+    var data = {
+      token: chance.guid(),
+      discount: req.body.discount,
+      comments: req.body.comments,
+      hasBeenUsed: false
     }
+    Token.create(data).exec(function (err, token) {
+      if (err) {
+        return res.badRequest();
+      } else {
+        res.json(token);
+      }
+    });
+  },
+  createCoupon: function (req, res) {
+    var data = {
+      name: req.body.name,
+      discount: req.body.discount,
+      comments: req.body.comments,
+      code: req.body.code,
+      isActive: true
+    };
+    CouponService.checkDuplicates(req.body.code, function (err, result) {
+      if (err || !result) {
+        res.badRequest("DUPLICATE");
+      } else {
+        Coupon.create(data).exec(function (err, coupon) {
+          if (err || !coupon) {
+            res.badRequest("DATABASE_ERR");
+          } else {
+            res.ok(coupon);
+          }
+        });
+      }
+    });
+  },
+  getToken: function (req, res) {
+    Token.findOne({
+      token: req.params.token
+    }).exec(function (err, token) {
+      if (err || !token) {
+        res.badRequest();
+      } else {
+        res.json(token);
+      }
+    });
+  },
+  getCouponMetadata: function (req, res) {
+    MetaService.getCouponMetadata(req.query.limit, req.query.where, function (result) {
+      res.json(result);
+    });
+  },
+  getCouponModel: function (req, res) {
+    res.json(new ModelService.Coupon());
+  }
 }
--- SwyftAPI/api/services/CouponService.js
+++ SwyftAPI/api/services/CouponService.js
@@ -1,104 +1,118 @@
 var Q = require('q');
 
 module.exports = {
-    getToken: function(code) {
-        return Q.promise(function(resolve, reject) {
-            Token.findOne({ token: code }).exec(function(err, token) {
-                if (!token) {
-                    reject(err);
-                } else if(token.hasBeenUsed) {
-                    reject(err);
-                } else {
-                    resolve(token);
-                }
-            });
-        });
-    },
-    getCoupon: function(code) {
-        return Q.promise(function(resolve, reject) {
-            Coupon.findOne({ code: code }).exec(function(err, coupon) {
-                if (err || !coupon) {
-                    reject(err);
-                } else if(!coupon.isActive) {
-                    reject(err);
-                } else {
-                    resolve(coupon);
-                }
-            });
-        });
-    },
-    getDiscount: function(code, cb) {
-        Q.allSettled([this.getToken(code), this.getCoupon(code)]).spread(function(token, coupon) {
-            if (token.state === 'fulfilled') {
-                cb(null, token.value);
-            } else if (coupon.state === 'fulfilled') {
-                cb(null, coupon.value);
-            } else {
-                cb(true);
-            }
-        });
-    },
-    processToken: function(order, cb) {
-        if(order.token) {
-            Token.findOne({ token: order.token }).exec(function(err, token) {
-                if(!token) {
-                    return cb(false);
-                }
-                if(token.hasBeenUsed) {
-                    return cb(false);
-                }
-                else {
-                    order.actualAmount *= token.discount;
-                    order.actualAmount = Math.round(order.actualAmount * 100) / 100;
-                    Token.update({ id: token.id }, { hasBeenUsed: true, orderId: order.id, usedBy: order.userId }).exec(function(err) {
-                        delete order.token;
-                        order.tokenId = token.id;
-                        cb(order);
-                    });
-                }
-            });
-        }
-        else {
-            delete order.token;
-            cb(order);
-        }
-    },
-    processCoupon: function(order, cb) {
-        if (order.coupon) {
-            Coupon.findOne({ code: order.coupon }).exec(function(err, coupon) {
-                if (err || !coupon) {
-                    return cb(false);
-                } else if(!coupon.isActive) {
-                    return cb(false);
-                } else {
-                    order.actualAmount *= coupon.discount;
-                    order.actualAmount = Math.round(order.actualAmount * 100) / 100;
-                    delete order.coupon;
-                    order.couponId = coupon.id;
-                    cb(order);
-                }
-            });
+  getToken: function (code) {
+    return Q.promise(function (resolve, reject) {
+      Token.findOne({
+        token: code
+      }).exec(function (err, token) {
+        if (!token) {
+          reject(err);
+        } else if (token.hasBeenUsed) {
+          reject(err);
         } else {
-            delete order.coupon;
-            cb(order);
+          resolve(token);
         }
-    },
-    checkDuplicates: function(code, cb) {
-        Coupon.find({ isActive: true }).exec(function(err, coupons) {
-            if (err || !coupons) {
-                cb(null, true);
-            } else {
-                async.each(coupons, function(coupon, callback) {
-                    if(coupon) {
-                        if(coupon.code === code) {
-                            return cb(true);
-                        }
-                    }
-                    callback();
-                }, function(err) {
-                    cb(null, true);
-                });
-            }
-        });
+      });
+    });
+  },
+  getCoupon: function (code) {
+    return Q.promise(function (resolve, reject) {
+      Coupon.findOne({
+        code: code
+      }).exec(function (err, coupon) {
+        if (err || !coupon) {
+          reject(err);
+        } else if (!coupon.isActive) {
+          reject(err);
+        } else {
+          resolve(coupon);
+        }
+      });
+    });
+  },
+  getDiscount: function (code, cb) {
+    Q.allSettled([this.getToken(code), this.getCoupon(code)]).spread(function (token, coupon) {
+      if (token.state === 'fulfilled') {
+        cb(null, token.value);
+      } else if (coupon.state === 'fulfilled') {
+        cb(null, coupon.value);
+      } else {
+        cb(true);
+      }
+    });
+  },
+  processToken: function (order, cb) {
+    if (order.token) {
+      Token.findOne({
+        token: order.token
+      }).exec(function (err, token) {
+        if (!token) {
+          return cb(false);
+        }
+        if (token.hasBeenUsed) {
+          return cb(false);
+        } else {
+          order.actualAmount *= token.discount;
+          order.actualAmount = Math.round(order.actualAmount * 100) / 100;
+          Token.update({
+            id: token.id
+          }, {
+            hasBeenUsed: true,
+            orderId: order.id,
+            usedBy: order.userId
+          }).exec(function (err) {
+            delete order.token;
+            order.tokenId = token.id;
+            cb(order);
+          });
+        }
+      });
+    } else {
+      delete order.token;
+      cb(order);
     }
+  },
+  processCoupon: function (order, cb) {
+    if (order.coupon) {
+      Coupon.findOne({
+        code: order.coupon
+      }).exec(function (err, coupon) {
+        if (err || !coupon) {
+          return cb(false);
+        } else if (!coupon.isActive) {
+          return cb(false);
+        } else {
+          order.actualAmount *= coupon.discount;
+          order.actualAmount = Math.round(order.actualAmount * 100) / 100;
+          delete order.coupon;
+          order.couponId = coupon.id;
+          cb(order);
+        }
+      });
+    } else {
+      delete order.coupon;
+      cb(order);
+    }
+  },
+  checkDuplicates: function (code, cb) {
+    Coupon.find({
+      isActive: true
+    }).exec(function (err, coupons) {
+      if (err || !coupons) {
+        cb(null, true);
+      } else {
+        async.each(coupons, function (coupon, callback) {
+          if (coupon) {
+            if (coupon.code === code) {
+              return cb(true);
+            }
+          }
+          callback();
+        }, function (err) {
+          cb(null, true);
+        });
+      }
+    });
+  }
 }
