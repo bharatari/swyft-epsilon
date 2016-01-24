6f717405da5c0a6fa214a2a48abc63b88d4f27ff
Bharat Arimilli <bbarimilli@outlook.com>
2016-01-24 11:58:19 -0600

    Edit dynamic-price

--- SwyftOnline/app/components/dynamic-price.js
+++ SwyftOnline/app/components/dynamic-price.js
@@ -4,10 +4,10 @@ import loginUtils from 'swyft-epsilon-online/utils/login-utils';
 
 export default Ember.Component.extend({  
     classNames: ['inline'],
-    discountedAmount: Ember.computed('price', 'finalAmount',function() {
+    discountedAmount: Ember.computed('price', 'finalAmount', function() {
         return Math.round((this.get('price') - this.get('finalAmount')) * 100) / 100;
     }),
-    computePrice: Ember.observer('price', 'discount', function() { 
+    computePrice: Ember.observer('price', 'discount', function() {
         if(this.get('discount')) {
             this.set('finalAmount', this.get('price') * this.get('discount'));
         }
