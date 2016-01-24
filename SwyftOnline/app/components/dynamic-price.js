ee60aa718aaf34dba0c34360a89aa02c737b76ad
Bharat Arimilli <bbarimilli@outlook.com>
2016-01-24 11:52:16 -0600

    Make CouponService, dynamic-price consistent with master

--- SwyftOnline/app/components/dynamic-price.js
+++ SwyftOnline/app/components/dynamic-price.js
@@ -4,27 +4,17 @@ import loginUtils from 'swyft-epsilon-online/utils/login-utils';
 
 export default Ember.Component.extend({  
     classNames: ['inline'],
-    /*
-    finalAmount: Ember.computed('price', 'discount', function() {
-        if (this.get('discount')) {
-            return this.get('price') * this.get('discount');
-        }
-        else {
-            return this.get('price');
-        }
-    }),*/
-    computeDiscounted: function() {
-        this.computePrice();
-        this.set('discountedAmount', Math.round((this.get('price') - this.get('finalAmount')) * 100) / 100);
-    },
-    computePrice: function() {        
+    discountedAmount: Ember.computed('price', function() {
+        return Math.round((this.get('price') - this.get('finalAmount')) * 100) / 100;
+    }),
+    computePrice: Ember.observer('price', 'discount', function() {        
         if(this.get('discount')) {
             this.set('finalAmount', this.get('price') * this.get('discount'));
         }
         else {
             this.set('finalAmount', this.get('price'));
         }
-    },
+    }),
     tokenValueChanged: Ember.observer('tokenValue', 'tokenValid', function() {
         var self = this;
         if (this.get('tokenValid') && this.get('tokenValue')) {
@@ -39,7 +29,6 @@ export default Ember.Component.extend({
             // FIXME finalAmount doesn't update properly on view when discount
             // goes back to null
             this.set('discount', null);
-            this.computeDiscounted();
         }
     })
 });
