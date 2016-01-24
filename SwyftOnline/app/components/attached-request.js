03c08775f140d516518581feb4147267de528d9b
Bharat Arimilli <bbarimilli@outlook.com>
2016-01-24 01:02:15 -0600

    Moved back to normal anonymous functions for Ember.computed and Ember.observer

--- SwyftOnline/app/components/attached-request.js
+++ SwyftOnline/app/components/attached-request.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 import itemUtils from 'swyft-epsilon-online/utils/item-utils';
 
 export default Ember.Component.extend({
-  displayChange: Ember.observer('item.itemOptions.@each.value', () => {
+  displayChange: Ember.observer('item.itemOptions.@each.value', function () {
     this.set('display', itemUtils.checkAttachedProperty(this.get('request'), this.get('item')));
   }),
   display: false,
